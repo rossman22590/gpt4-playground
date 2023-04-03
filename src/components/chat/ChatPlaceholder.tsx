78c8f8160ae380942c44606fea8b32684d1916e5
Ross Cohen <rosscohen1990@gmail.com>
2023-04-03 00:48:31 -0400

    Update ChatPlaceholder.tsx

--- src/components/chat/ChatPlaceholder.tsx
+++ src/components/chat/ChatPlaceholder.tsx
@@ -9,7 +9,7 @@ export default function ChatPlaceholder({}: Props) {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="max-w-3xl p-4 text-center text-primary">
-        <h1 className="text-4xl font-medium">GPT-4 Playground</h1>
+        <h1 className="text-4xl font-medium">Tutor AI Playground</h1>
         <p className="mt-4 text-lg">
 Tutor AI playground that allows you to switch between GPT 3.5 and GPT 4 and has the freedom to process any prompt
         </p>
